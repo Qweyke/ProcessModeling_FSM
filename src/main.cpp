7423aa636e3f0623b11bbcaba9b3336322b39fe8
Qweyke <qweyke@gmail.com>
2024-10-21 18:54:43 +0500

    main redone

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,85 @@
 #include <iostream>
-// #include <string>
+#include <string>
+
+constexpr uint16_t ROWS          = 4;
+constexpr uint16_t COLUMNS       = 3;
+constexpr uint16_t INPUT_SIGNALS = 4;
+
+uint16_t currentState = 0;
+uint16_t inSignal     = 0;
+
+enum States
+{
+    IDLE,
+    OPEN,
+    HANDLING
+};
+
+uint16_t nextStates[ROWS][COLUMNS] = { { OPEN, OPEN, HANDLING },
+                                       { IDLE, HANDLING, HANDLING },
+                                       { IDLE, HANDLING, IDLE },
+                                       { IDLE, IDLE, IDLE } };
+
+std::string outputSignals[ROWS][COLUMNS]
+    = { { "TCP. Connected", "TCP. Already connected", "TCP. Already connected" },
+        { "HTTP. No connection",
+          "HTTP. Request recieved, handling",
+          "HTTP. Request recieved, setting in queue" },
+        { "HTTP. No connection",
+          "HTTP. Request handled, sending",
+          "HTTP. Answer sent, waiting TCP-connection" },
+        { "TCP. Waiting", "TCP. Closed, waiting", "TCP. Closed, waiting" } };
+
+std::string inputSignals[INPUT_SIGNALS] = { "[TCP. Connection requested]",
+                                            "[HTTP. Recieved]",
+                                            "[HTTP. Handled]",
+                                            "[TCP.Connection interrupted]" };
+
+std::string states[ROWS]
+    = { "TCP. Waiting connection", "TCP. Connection opened", "HTTP. Handling" };
+
+
+void displaySignals(std::string signals[], size_t size)
+{
+    std::string output;
+    output.reserve(size * 40);
+    std::cout << "Input signals:\n";
+
+    int i = 0;
+    for (i; i < size; i++)
+    {
+        output += std::to_string(i) + " - " + (signals[i]) + (";\n");
+    }
+
+    output += (std::to_string(i)) + " - Show hint;\n" + (std::to_string(i + 1))
+              + " - Exit program. \n\n";
+    std::cout << output;
+}
+
 
 int main()
 {
-    const int rows    = 4;
-    const int columns = 3;
-
-    int currentState = 0;
-    int inSignal     = 0;
-
-    enum States
-    {
-        IDLE,
-        OPEN,
-        HANDLING
-    };
-
-    int nextStates[rows][columns] = { { OPEN, OPEN, HANDLING },
-                                      { IDLE, HANDLING, HANDLING },
-                                      { IDLE, HANDLING, IDLE },
-                                      { IDLE, IDLE, IDLE } };
-
-    std::string outputSignals[rows][columns]
-        = { { "TCP. Connected", "TCP. Already connected", "TCP. Already connected" },
-            { "HTTP. No connection",
-              "HTTP. Request recieved, handling",
-              "HTTP. Request recieved, setting in queue" },
-            { "HTTP. No connection",
-              "HTTP. Request handled, sending",
-              "HTTP. Answer sent, waiting TCP-connection" },
-            { "TCP. Waiting", "TCP. Closed, waiting", "TCP. Closed, waiting" } };
-
-    std::string stateNames[rows]
-        = { "TCP. Waiting connection", "TCP. Connection opened", "HTTP. Handling" };
-
-    std::cout << "Current state: " << currentState << " - " << stateNames[currentState] << "\n\n";
-    std::cout
-        << "Input signals:\n 0 - [TCP. Connection requested];\n 1 - [HTTP. Recieved];\n 2 - [HTTP. Handled];\n 3 - [TCP.Connection interrupted];\n 4 - Show hint;\n 5 - Exit program. \n\n";
+    std::cout << "Current state: " << currentState << " - " << states[currentState] << "\n\n";
+    displaySignals(inputSignals, INPUT_SIGNALS);
 
     while (1)
     {
         std::cout << "Signal:";
         std::cin >> inSignal;
         std::cout << "\n";
-
         if ((inSignal < 4) && (inSignal >= 0))
         {
             currentState = nextStates[inSignal][currentState];
 
-            std::cout << "Current state: " << currentState << " - " << stateNames[currentState]
+            std::cout << "Current state: " << currentState << " - " << states[currentState]
                       << std::endl;
             std::cout << "Output signal: [" << outputSignals[inSignal][currentState] << "]\n\n";
         }
         else if (4)
         {
-            std::cout << "Current state: " << currentState << " - " << stateNames[currentState]
+            std::cout << "Current state: " << currentState << " - " << states[currentState]
                       << "\n\n";
-            std::cout
-                << "Input signals: 0 - [TCP. Connection requested]; 1 - [HTTP. Recieved]; 2 - [HTTP. Handled]; 3 - [TCP.Connection interrupted]; 4 - Show hint; 5 - Exit program. \n\n";
+            displaySignals(inputSignals, INPUT_SIGNALS);
         }
 
         else if (5)
